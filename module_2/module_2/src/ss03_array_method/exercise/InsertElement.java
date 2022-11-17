c28ff82493b890a625ac78296f5a912cf55472d6
Nguyen Ngoc Hieu <a12345@12345s-MacBook-Pro.local>
2022-11-17 20:53:55 +0700

    first commit

--- module_2/module_2/src/ss03_array_method/exercise/InsertElement.java
+++ module_2/module_2/src/ss03_array_method/exercise/InsertElement.java
@@ -1,47 +1,28 @@
 package ss03_array_method.exercise;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class InsertElement {
-    private static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-        int i;
-        int[] array1 = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-        System.out.println("nhập phần tử cần chèn: ");
-        int k = scanner.nextInt();
-        System.out.println("nhập vị trí chèn phần tử : ");
-        int index = scanner.nextInt();
-        for (i = 0; i < 10; i++) {
-            if(index<=1&&index>=9){
-                System.out.println("không chèn phần tử vào được ");
-            }else{
-                array1=insert(array1,k);
-            }
-        }
-        System.out.println("Mảng còn lại sau khi chèn phần tử " + k + " là: ");
-        for (i = 0; i < 11; i++) {
-            System.out.print(array1[i] + "\t");
-        }
-    }
-    public static int [] insert(int [] array1, int k) {
-        int index = array1.length - 1;
-        int tempIndex = array1.length;
-        int [] tempArr = new int [tempIndex + 1];
-        boolean inserted = false;
-
-        for (int i = tempIndex; i >= 0; i--) {
-            if (index > -1 && array1[index] > k) {
-                tempArr[i] = array1[index--];
-            } else {
-                if (!inserted) {
-                    tempArr[i] = k;
-                    inserted = true;
-                } else {
-                    tempArr[i] = array1[index--];
+        Scanner sc = new Scanner(System.in);
+        int[] array = {10, 4, 6, 7, 8, 0, 0, 0, 0, 0};
+        int n,index;
+        do {
+            System.out.println("Nhập giá trị phần tử cần chèn :");
+            n = sc.nextInt();
+            System.out.println("Nhập vị trí chèn :");
+            index = sc.nextInt();
+        } while (index < 0 || index >= (array.length - 1));
+        for (int i = 0; i < array.length - 1; i++) {
+            if (i == index) {
+                for (int j = array.length - 1; j > i; j--) {
+                    array[j] = array[j - 1];
                 }
+                array[index] = n;
             }
         }
-        return tempArr;
+        System.out.println("Mảng sau khi chèn phần tử là: ");
+        System.out.println(Arrays.toString(array));
     }
-
 }
